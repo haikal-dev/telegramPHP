6d154a0dc04fde73b554f5c9f37334df6f8f9b67
mhaikalazizan <mhaikalazizan@gmail.com>
2022-01-24 17:56:15 +0800

    Updated some docs in core/telegram.php

--- core/telegram.php
+++ core/telegram.php
@@ -11,17 +11,50 @@
 
 class TelegramModel
 {
+	/**
+	 * $url
+	 * 
+	 * Telegram Bot API must have URL and its token
+	 * to access (push/receive)
+	 * 
+	 */
 	public $url = 'https://api.telegram.org/botXXXXXXXXXXXXX';
+	
+	/**
+	 * $users
+	 * 
+	 * who are users that need bot to send with.
+	 * 
+	 */
 	private $users = [];
 	
 	public function __construct(){
 		
 	}
 	
+	/**
+	 * $setChatId
+	 * 
+	 * by using this method will push array of '$users'
+	 * property.
+	 * this allow our bot to send multi-user simultaneously.
+	 * 
+	 */
 	public function setChatId($chatId){
 		array_push($this->users, $chatId);
 	}
 	
+	/**
+	 * $getData
+	 * 
+	 * this method allow php to received data
+	 * sent by telegram user in json format.
+	 * 
+	 * with this method, php can response back
+	 * based on data received to telegram user
+	 * through our bot.
+	 * 
+	 */
 	public function getData(){
 		$request = file_get_contents('php://input');
 		$request = json_decode($request, TRUE);
@@ -36,10 +69,24 @@ class TelegramModel
 		return $data;
 	}
 	
+	/**
+	 * $setRobot
+	 * 
+	 * this method allow us to update our full telegram
+	 * api url with our bot token.
+	 * 
+	 */
 	public function setRobot($robot_id){
 		$this->url = "https://api.telegram.org/bot" . $robot_id;
 	}
 	
+	/**
+	 * $showChat
+	 * 
+	 * For debugging purpose. Better use $getData instead of
+	 * $showChat
+	 * 
+	 */
 	public function showChat(){
 		$data = $this->url . '/getUpdates';
 		$json = json_decode(file_get_contents($data), true);
@@ -49,6 +96,13 @@ class TelegramModel
 		echo '</pre>';
 	}
 	
+	/**
+	 * $sendPhoto
+	 * 
+	 * Let php to send photo to user
+	 * through our bot.
+	 * 
+	 */
 	public function sendPhoto($file_id){
 		foreach($this->users as $chatter){
 			$data = $this->url . '/sendPhoto?chat_id=' . $chatter . '&photo=' . $file_id;
@@ -57,6 +111,15 @@ class TelegramModel
 		}
 	}
 	
+	/**
+	 * $send
+	 * 
+	 * let php to send message to multi/user through
+	 * telegram bot
+	 * 
+	 * Available $method = 'sendMessage'
+	 * 
+	 */
 	public function send($method, $message, $inline = [])
 	{
 		$encoded = json_encode($inline);
